fcfcb26dd5c895a6f9342892728ad192d983cbaf
lucasAmoroso <lucasAmoroso@github.com>
2016-04-27 19:14:00 -0300

    actualice consola, pero no funciona

---
+++ consola/src/basicFunciones.h
@@ -0,0 +1,18 @@
+/*
+ * basicFunciones.h
+ *
+ *  Created on: 27/4/2016
+ *      Author: utnso
+ */
+
+#ifndef SRC_BASICFUNCIONES_H_
+#define SRC_BASICFUNCIONES_H_
+
+void crearSocket(int* socketacrear);
+void abrirPuerto(int socket, struct sockaddr_in* adress);
+void escucharConexiones(int socket, int colamax);
+void aceptarConexion(int* socketnuevo, int socketescuchador,struct sockaddr_in* other_adress);
+void enviarMensaje(int socketDestino, char* mensaje);
+char* recibirMensaje(int socketCliente);
+
+#endif /* SRC_BASICFUNCIONES_H_ */
--- consola/src/consola.c
+++ consola/src/consola.c
@@ -14,7 +14,7 @@
 #include <string.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
-#include "socketServer.h"
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -29,7 +29,7 @@
 
 int main(void) {
 
-
+/*
 	struct sockaddr_in nucleo_addr_proc;
 		nucleo_addr_proc.sin_family = AF_INET;
 		nucleo_addr_proc.sin_addr.s_addr = INADDR_ANY;
@@ -40,13 +40,17 @@ int main(void) {
 		int cliente = socket(AF_INET, SOCK_STREAM, 0);
 			if (connect(cliente, (void*) &nucleo_addr_proc, sizeof(nucleo_addr_proc)) != 0) {
 				perror("No se pudo conectar");
-				return 1;}
+				return 1;}*/
+	struct cliente clienteConsola;
+	clienteConsola=crearCliente(8080,INADDR_ANY);
+	conectarConServidor(clienteConsola);
 	while (1) {
 		char mensaje[SOMAXCONN];
 		scanf("%s", mensaje);
 
-		send(cliente, mensaje, strlen(mensaje), 0);
+		send((clienteConsola.socketCliente), mensaje, strlen(mensaje), 0);
 	}
+
 
 	return 0;
 }
