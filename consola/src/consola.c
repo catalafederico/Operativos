1f2c4360e16eb293e8de7c26b1849ebc11a04bc6
lucasAmoroso <lucasAmoroso@github.com>
2016-05-09 10:35:40 -0300

    round robin en nucleo

--- consola/src/consola.c
+++ consola/src/consola.c
@@ -28,18 +28,6 @@
 
 int main(void) {
 
-/*
-	struct sockaddr_in nucleo_addr_proc;
-		nucleo_addr_proc.sin_family = AF_INET;
-		nucleo_addr_proc.sin_addr.s_addr = INADDR_ANY;
-		nucleo_addr_proc.sin_port = htons(8080);
-
-
-
-		int cliente = socket(AF_INET, SOCK_STREAM, 0);
-			if (connect(cliente, (void*) &nucleo_addr_proc, sizeof(nucleo_addr_proc)) != 0) {
-				perror("No se pudo conectar");
-				return 1;}*/
 	struct cliente clienteConsola;
 	clienteConsola=crearCliente(8080,"127.0.0.1");
 	conectarConServidor(clienteConsola);
--- nucleo/src/nucleo.c
+++ nucleo/src/nucleo.c
@@ -400,5 +400,54 @@ t_reg_config get_config_params(void){
 	config_destroy(archivo_config);
 	return reg_config;
 }
+void RoundRobin( int quantum){
+int count,j,n,tiempo,restante,flag=0;
+  int tiempo_espera=0,tiempo_cambio=0,at[10],bt[10],rt[10];
+  printf("Enter Total Process:\t ");
+  scanf("%d",&n);
+  restante=n;
+  for(count=0;count<n;count++)
+  {
+    printf("Enter Arrival Time and Burst Time for Process Process Number %d :",count+1);
+    scanf("%d",&at[count]);
+    scanf("%d",&bt[count]);
+    rt[count]=bt[count];
+  }
+  printf("Enter Time Quantum:\t");
+  scanf("%d",&quantum);
+  printf("\n\nProcess\t|Turnaround Time|Waiting Time\n\n");
 
+  for(tiempo=0,count=0;restante!=0;)
+  {
+    if(rt[count]<=quantum && rt[count]>0)
+    {
+      tiempo+=rt[count];
+      rt[count]=0;
+      flag=1;
+    }
+    else if(rt[count]>0)
+    {
+      rt[count]-=quantum;
+      tiempo+=quantum;
+    }
+    if(rt[count]==0 && flag==1)
+    {
+      restante--;
+      printf("P[%d]\t|\t%d\t|\t%d\n",count+1,tiempo-at[count],tiempo-at[count]-bt[count]);
+      tiempo_espera+=tiempo-at[count]-bt[count];
+      tiempo_cambio+=tiempo-at[count];
+      flag=0;
+    }
+    if(count==n-1)
+      count=0;
+    else if(at[count+1]<=tiempo)
+      count++;
+    else
+      count=0;
+  }
+  printf("\nAverage Waiting Time= %f\n",tiempo_espera*1.0/n);
+  printf("Avg Turnaround Time = %f",tiempo_cambio*1.0/n);
 
+  return;
+
+}
