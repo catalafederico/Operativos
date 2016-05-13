40a423c9bf0d97709f9b6e5f9d0e2e1e54ba3c20
utnso <ezec09@hotmail.com>
2016-05-13 19:22:00 -0300

    arreglado el tema del archivo

--- consola/src/consola.c
+++ consola/src/consola.c
@@ -8,7 +8,6 @@
  ============================================================================
  */
 
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -24,41 +23,45 @@
 #include <string.h>
 
 int main(void) {
-	char* ch;
+	int ch;
 	char* buffer;
 	int size;
 	FILE *archivoAnsisop;
-     archivoAnsisop = fopen("/home/utnso/workspace/tp-2016-1c-Explosive-code/TestParser/facil.ansisop","r");
+	archivoAnsisop =
+			fopen(
+					"/home/utnso/Escritorio/TPOperativos/tp-2016-1c-Explosive-code/TestParser/facil.ansisop",
+					"r");
 
-	 if(  archivoAnsisop== NULL )
-		   {
-		      perror("Error al tratar de leer archivo");
-		      exit(EXIT_FAILURE);
-		   }
+	if (archivoAnsisop == NULL) {
+		perror("Error al tratar de leer archivo");
+		exit(EXIT_FAILURE);
+	}
 
+	fseek (archivoAnsisop , 0 , SEEK_END);
+	size = ftell (archivoAnsisop);
+	rewind (archivoAnsisop);
+	//es mejor poner size en vez del puntero q estaba antes, le puse un mas 5 por las dudas
+	buffer = malloc(size+5);
 
-	 fseek (archivoAnsisop , 0 , SEEK_END);
-	 size = ftell (archivoAnsisop);
-	 rewind (archivoAnsisop);
-	 buffer = (char*) malloc (sizeof(char)*size);
+	struct cliente clienteConsola;
+	clienteConsola = crearCliente(8080, "127.0.0.1");
+	//conectarConServidor(clienteConsola);
+	/*char* msje="Hola";
+	 char* handshake= hacerHandShake_server(clienteConsola.socketServer,msje);
+	 printf("%p/n",handshake);*/
 
-	 struct cliente clienteConsola;
-	 clienteConsola=crearCliente(8080,"127.0.0.1");
-     conectarConServidor(clienteConsola);
-/*	char* msje="Hola";
-	char* handshake= hacerHandShake_server(clienteConsola.socketServer,msje);
-	printf("%p/n",handshake);*/
+	//fgetc devuelve un int, quizas por eso tira error al acceder a memoria
+	ch = fgetc(archivoAnsisop);
+	while ((ch) != EOF) {
+		//Le paso la direccion de ch xq strcat recibe dos punteros char
+		strcat(buffer, (char*) &ch);
+		ch = fgetc(archivoAnsisop);
+	}
 
-
-
-	  while( ( ch = fgetc(archivoAnsisop) ) != EOF ){
-	  strcat(buffer,ch);
-
-		  }
-
-       send((clienteConsola.socketCliente), buffer, strlen(buffer), 0);
-       close(clienteConsola.socketCliente);
-       free(buffer);
+	//al usar strlen, no cuenta los espacios asi q hay q ve otra forma, quizas usando size de antes, pero nose
+	send((clienteConsola.socketCliente), buffer, strlen(buffer), 0);
+	close(clienteConsola.socketCliente);
+	free(buffer);
 
 	return 0;
 }
