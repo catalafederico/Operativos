faa77f1cea95565f0e37ee05033dd42eb60ce26b
Brian Ezeiza <brianezeiza@hotmail.com>
2016-05-17 20:10:23 -0300

    Defini las funciones primitivas del parser

--- cpu/src/estructuras.h
+++ cpu/src/estructuras.h
@@ -8,11 +8,29 @@
 #ifndef ESTRUCTURAS_H_
 #define ESTRUCTURAS_H_
 
-typedef struct{
+typedef struct {
 	int pagina;
 	int offset;
 	int tamanio;
 }__attribute__((packed))
 solcUMC;
+
+typedef struct {
+	int pagina;
+	int offset;
+	int tamanio;
+	int valor;
+}__attribute__((packed))
+almUMC;
+
+typedef struct{
+	int id;
+	int ip;
+	int sp;
+
+}pcb;
+
+
+
 
 #endif /* ESTRUCTURAS_H_ */
