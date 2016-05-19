179c52e98d86e3883d345fa46230df8a1e9db58d
lucasAmoroso <lucas.-94@outlook.com>
2016-05-19 09:55:24 -0300

    arme para que consola reciba como parametro una ruta del archivo, todavia  no se bien como probarlo

--- consola/src/consola.c
+++ consola/src/consola.c
@@ -23,14 +23,22 @@
 #include <commons/txt.h>
 #include <string.h>
 
-int main(void) {
+int main(int argc, char **argv) {
+/*	  int index;
+	  for(index = 0; index < argc; index++) {
+	    printf("  Parametro %d: %s\n", index, argv[index]);
+	  }*/
+
 	int ch;
 	char* buffer;
 	int size;
 	char* buff;
+	char* rutaArchivo;
+	rutaArchivo=argv[1];
 	FILE *archivoAnsisop;
-	archivoAnsisop =fopen("/home/utnso/Escritorio/TPOperativos/tp-2016-1c-Explosive-code/consola/facil.ansisop","r");
-
+//	archivoAnsisop =fopen("/home/utnso/Escritorio/TPOperativos/tp-2016-1c-Explosive-code/consola/facil.ansisop","r");
+//hay que abrirlo con el gcc ejecutarlo y pasarle los parametros el primer parametro(argv[0]) es el programa y el otro la rutadearchivo
+	archivoAnsisop =fopen(rutaArchivo,"r");
 	if (archivoAnsisop == NULL) {
 		perror("Error al tratar de leer archivo");
 		exit(EXIT_FAILURE);
